fe11dd476670cbdb2ce56fa511f387489364df92
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-01-16 07:39:39 +0530

    add dis

---
+++ dist/build/precache-manifest.7c265a53ea887178452f277022642362.js
@@ -0,0 +1,92 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/css/04d54418e55f243fd81d.css"
+  },
+  {
+    "url": "/css/5efcb89f13652aed677d.css"
+  },
+  {
+    "url": "/css/6c7aa8bf66adc6ceec04.css"
+  },
+  {
+    "url": "/css/7d838dc845e3a2f66f9a.css"
+  },
+  {
+    "url": "/images/18bb7fc8246d46ebb4cb14f4624b841f-mosquiteer-icon.png"
+  },
+  {
+    "url": "/images/2dd0be211389afdace8eb074c39e0d93-pwa-icon-512x512.png"
+  },
+  {
+    "url": "/images/3e5eb24fafc5939b3d3c2f20e9e1a052-mosquiteer.png"
+  },
+  {
+    "url": "/images/4c7800b9753059d9d718cd097a1e8e1d-home-splash-screen.png"
+  },
+  {
+    "url": "/images/69dea52405c3aad0e969fb69900bf0a0-login.png"
+  },
+  {
+    "url": "/images/923eeec120e716e233cf8b6b2b431b2b-features.png"
+  },
+  {
+    "url": "/images/9ebde8a259df9506b435f04d04fe27ab-skeleton-loading.png"
+  },
+  {
+    "url": "/images/b5495037ab2160c4fb4cafbfd4416939-pic-header-mathworks-logo.svg"
+  },
+  {
+    "url": "/images/d7d9f590276521036f2f06a67f426cb0-pwa-icon-192x192.png"
+  },
+  {
+    "url": "/images/f4f46ae5199c62c66f7ce5a0d7ab1f31-mosquiteer-512x512.png"
+  },
+  {
+    "url": "/js/04c92edd05ba6249cd0e.js"
+  },
+  {
+    "url": "/js/04d54418e55f243fd81d.js"
+  },
+  {
+    "url": "/js/09100dfe6e0a12f61adf.js"
+  },
+  {
+    "url": "/js/11f1e1dcc71ff2427975.js"
+  },
+  {
+    "url": "/js/137d0875130435b45dbb.js"
+  },
+  {
+    "url": "/js/186b8817ded6da8f8c93.js"
+  },
+  {
+    "url": "/js/5d069e7affe9ebb2904b.js"
+  },
+  {
+    "url": "/js/5efcb89f13652aed677d.js"
+  },
+  {
+    "url": "/js/6c7aa8bf66adc6ceec04.js"
+  },
+  {
+    "url": "/js/7d838dc845e3a2f66f9a.js"
+  },
+  {
+    "url": "/js/9c903b4129f77245918f.js"
+  },
+  {
+    "url": "/js/acc3d3f41cfc10cba123.js"
+  },
+  {
+    "url": "/js/b3b5c836ceaa8642419c.js"
+  },
+  {
+    "url": "/js/ba7bc35f0d4d1468afc9.js"
+  },
+  {
+    "url": "/js/d8527872ddafbbcd01b2.js"
+  },
+  {
+    "url": "/js/ffd7f998bd820a4d301a.js"
+  }
+]);
---
+++ dist/build/sw.js
@@ -0,0 +1,80 @@
+self.paw__env={"PAW_CACHE":"true","PAW_VERBOSE":"false","PAW_ENV":"production","PAW_CONFIG_PATH":"/Users/VISHNU/mosquiteer-master/pawconfig.json","PAW_HOT":"false"};importScripts("/precache-manifest.7c265a53ea887178452f277022642362.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// eslint-disable-next-line
+const serviceWorker = self;
+
+serviceWorker.addEventListener('install', () => {
+  serviceWorker.skipWaiting();
+});
+serviceWorker.addEventListener('activate', () => {
+  serviceWorker.clients.claim();
+});
+
+serviceWorker.workbox.setConfig({
+  debug: serviceWorker.paw__env.PAW_ENV !== 'production',
+});
+
+const getOfflineHtml = () => {
+  const scripts = serviceWorker.paw__offline_assets.filter(a => a.endsWith('.js')).map(js => `<script type="text/javascript" src="${js}" async></script>`).join('');
+  return `<!DOCTYPE html><html><head></head><body><div id="${serviceWorker.paw__injected_variables.clientRootElementId}"></div>${scripts}</body></html>`;
+};
+
+serviceWorker.workbox.routing.registerRoute(
+  new RegExp(`^${serviceWorker.location.origin}/.*__hmm_update.*`),
+  serviceWorker.workbox.strategies.networkOnly(),
+);
+
+const assetsRegExp = /\.(css|js|jpg|png|jpeg|gif|woff|woff2|ttf|eot|ico|mp4|avi)$/;
+
+const networkFirstHandler = serviceWorker.workbox.strategies.networkFirst();
+const cacheFirstHandler = serviceWorker.workbox.strategies.cacheFirst();
+const staleHandler = serviceWorker.workbox.strategies.staleWhileRevalidate();
+
+serviceWorker.workbox.routing.setDefaultHandler(({ event }) => {
+  const { request } = event;
+  const requestMethod = request.method.toUpperCase();
+
+  if (requestMethod !== 'GET') {
+    return fetch(event.request);
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) !== -1
+    && assetsRegExp.test(request.url)
+  ) {
+    return cacheFirstHandler.handle({ event });
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) === -1
+    && assetsRegExp.test(request.url)
+  ) {
+    return staleHandler.handle({ event });
+  }
+
+  if (
+    request.url.indexOf(serviceWorker.location.origin) !== -1
+    && request.headers.get('accept').indexOf('html') !== -1
+    && request.mode === 'navigate'
+  ) {
+    return networkFirstHandler.handle({ event }).then((response) => {
+      if (!response) {
+        return new Response(
+          getOfflineHtml(),
+          { headers: { 'Content-Type': 'text/html' } },
+        );
+      }
+      return response;
+    }).catch(() => new Response(
+      getOfflineHtml(),
+      { headers: { 'Content-Type': 'text/html' } },
+    ));
+  }
+
+  return networkFirstHandler.handle({ event });
+});
+
+// eslint-disable-next-line
+serviceWorker.workbox.precaching.precacheAndRoute(serviceWorker.__precacheManifest);
+
+;self.paw__offline_assets = ["/css/04d54418e55f243fd81d.css","/js/04d54418e55f243fd81d.js","/css/7d838dc845e3a2f66f9a.css","/js/7d838dc845e3a2f66f9a.js"];self.paw__injected_variables = {"workboxDebug":true,"port":"8080","host":"0.0.0.0","appRootUrl":"","cdnUrl":"","serverSideRender":true,"serviceWorker":true,"singlePageApplication":false,"asyncCSS":true,"polyfill":"internal","react":"internal","clientRootElementId":"app","assetsMaxAge":2592000000,"hstsEnabled":true,"hstsMaxAge":31536000,"hstsIncludeSubDomains":false,"hstsPreload":false,"noJS":false,"hashedRoutes":false,"resourcesBaseUrl":"/"};
