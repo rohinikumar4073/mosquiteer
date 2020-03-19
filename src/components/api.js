5c2afc5fd9220e53981081c0c7f677aed525615e
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-03-19 23:28:29 +0530

    deleting dist and fixing for backend url

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,10 @@
 let hostname = 'http://localhost:8080';
+if (window.location.hostname === 'mosquiteer.herokuapp.com') {
+    hostname = 'https://mosquiteer-backend.herokuapp.com/';
+}
 export default {
     getAllMosquitoBreeding: hostname + '/getAllMosquitoBreeding',
-    reportMosquitoBreeding: hostname +'/reportMosquitoBreeding',
-    getRSSFeed: hostname +'/getRSSFeed'
+    reportMosquitoBreeding: hostname + '/reportMosquitoBreeding',
+    getRSSFeed: hostname + '/getRSSFeed'
 
 }
