5ad489d32cb859ee721922d11eb6e36a0b809307
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-03-19 23:50:31 +0530

    fix  for api

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 let hostname = 'http://localhost:8080';
 if (window.location.hostname === 'mosquiteer.herokuapp.com') {
-    hostname = 'https://mosquiteer-backend.herokuapp.com/';
+    hostname = 'https://mosquiteer-backend.herokuapp.com';
 }
 export default {
     getAllMosquitoBreeding: hostname + '/getAllMosquitoBreeding',
