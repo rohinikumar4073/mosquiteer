0ddb6e2aadd95252afee211d2002bc61097c4e40
vishnu sandhireddy <vissu_nbkr@yahoo.com>
2020-03-25 01:03:34 +0530

    Added icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,14 +3,6 @@ import MathWorksLogo from '../resources/img/pic-header-mathworks-logo.svg';
 
 export default () => (
   <footer className="footer mosquiteer">
-    <div className="content has-text-centered">
-      <p>
-        <strong>Demo for Hyderabad Hack Day</strong>
-        <br />
-        <a href="https://www.mathworks.com/">
-          <img src={MathWorksLogo} alt="MathWorks" width="150" height="100" />
-        </a>
-      </p>
-    </div>
+   
   </footer>
 );
